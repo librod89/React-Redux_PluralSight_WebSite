c5039ac64e927de68058921b70ff7bbcf8a6bf0a
Libby <librod89@gmail.com>
2016-12-15 18:30:26 -0500

    Create ManageCoursesPage to add or update courses. Create async mock api calls to get course and author data.

---
+++ src/reducers/authorReducer.js
@@ -0,0 +1,11 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+export default function authorReducer(state = initialState.authors, action){ //ES6 default parameter
+  switch(action.type){
+    case types.LOAD_AUTHORS_SUCCESS:
+      return action.authors;
+    default:
+      return state;
+  }
+}
